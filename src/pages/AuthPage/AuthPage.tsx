08526af0ce2d3e754a01f63ad83787f1b764c5b7
Artem Shutov <artfergames@gmail.com>
2026-07-23 21:39:54 +0700

    feat: init structure

---
+++ src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,8 @@
+export default function AuthPage() {
+  return (
+    <>
+      <p>всем ку</p>
+      <p> тут рега и вход</p>
+    </>
+  );
+}
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+export default function MainPage() {
+  return (
+    <>
+      <p>всем привет</p>
+      <p>это главная</p>
+    </>
+  );
+}
